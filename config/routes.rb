481562c2301e769211a46232e222a14bae3bcc36
pseudokiing <jaideeking5@gmail.com>
2020-02-12 11:24:34 -0500

    Issue with PG Gem, Database not seeding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "articles#index"
   resources :comments
   resources :users
   resources :articles
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+for i in 1..50
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  name = "#{first_name} #{last_name}"
+  email = Faker::Internet.free_email(name)
+
+  user = User.new
+  user.email = email
+  user.first_name = first_name
+  user.last_name = last_name
+  user.street_address = Faker::Address.street_address
+  user.city = Faker::Address.city
+  user.province = Faker::Address.state
+  user.postal_code = Faker::Address.postcode
+  user.country = Faker::Address.country
+  if user.save
+    for ii in 1..50
+      article = Article.new
+      article.user = user
+      if article.save
+        for iii in 1..10
+          article.comment = Faker::Lorem.paragraph
+        end
+      end
+    end
+  end
+end
